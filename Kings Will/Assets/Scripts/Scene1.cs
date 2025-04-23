49b6b0a8ed75eb5bfa29780124d3b9977f4b3f00
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-04-23 13:03:22 +0530

    Scene 1 completed

--- Kings Will/Assets/Scripts/Scene1.cs
+++ Kings Will/Assets/Scripts/Scene1.cs
@@ -2,22 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System;
+using UnityEngine.SceneManagement;
 
 public class Scene1 : MonoBehaviour
 {
     [SerializeField] float fadeInScreenTime;
 
+    [Header("UI Elements")]
     public GameObject fadeInScreen;
-    public GameObject charKingJames;
     public GameObject textBox;
-    public GameObject continueButton;
     public TMP_Text textArea;
+    public GameObject continueButton;
     public TMP_Text charName;
+    public GameObject choicePanel;
 
+    [Header("Scene Characters")]
+    public GameObject charKingJames;
+    public GameObject charPrinceLuke;
+    public GameObject charGeneralRich;
+    public GameObject charPrincessAnna;
+
+    [Header("Miscellaneuos")]
     public float typeSpeed;
+
     string currText = "";
     private int eventCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -47,7 +57,7 @@ public class Scene1 : MonoBehaviour
         yield return new WaitUntil(() => currText == sentence);
         eventCount++;
         continueButton.SetActive(true);
-        
+
     }
 
     IEnumerator EventTwo()
@@ -73,18 +83,149 @@ public class Scene1 : MonoBehaviour
         eventCount++;
         continueButton.SetActive(true);
     }
-    
-    public void ContinueButton()
+
+    IEnumerator EventFour()
     {
-        if(eventCount == 1)
-        {
-            StartCoroutine(EventTwo());
-        }
-        else if(eventCount == 2)
-        {
-            StartCoroutine(EventThree());
-        }
+        // eventCount 3
+        charKingJames.SetActive(false);
+        charName.text = "Prince Luke";
+        textArea.text = "";
+        continueButton.SetActive(false);
+        textBox.SetActive(false);
+        charPrinceLuke.SetActive(true);
+        yield return new WaitForSeconds(2);
+        textBox.SetActive(true);
+        string sentence = "What is this all about?! Father.. I need some time..";
+        StartCoroutine(Type(sentence));
+        yield return new WaitUntil(() => currText == sentence);
+        eventCount++;
+        continueButton.SetActive(true);
     }
 
+    IEnumerator EventFive()
+    {
+        // eventCount 4
+        textArea.text = "";
+        continueButton.SetActive(false);
+        string sentence = "(Leaves for his room)";
+        StartCoroutine(Type(sentence));
+        yield return new WaitUntil(() => currText == sentence);
+        eventCount++;
+        continueButton.SetActive(true);
+
+    }
+
+    IEnumerator EventSix()
+    {
+        // eventCount 5
+        charPrinceLuke.SetActive(false);
+        charName.text = "Princess Anna";
+        textArea.text = "";
+        continueButton.SetActive(false);
+        textBox.SetActive(false);
+        charPrincessAnna.SetActive(true);
+        yield return new WaitForSeconds(2);
+        textBox.SetActive(true);
+        string sentence = "Wait father, I need to talk to you!";
+        StartCoroutine(Type(sentence));
+        yield return new WaitUntil(() => currText == sentence);
+        eventCount++;
+        continueButton.SetActive(true);
+    }
+
+    IEnumerator EventSeven()
+    {
+        // eventCount 6
+        textArea.text = "";
+        continueButton.SetActive(false);
+        string sentence = "(Goes after the King)";
+        StartCoroutine(Type(sentence));
+        yield return new WaitUntil(() => currText == sentence);
+        eventCount++;
+        continueButton.SetActive(true);
+    }
+
+
+    IEnumerator EventEight()
+    {
+        // eventCount 7
+        charPrincessAnna.SetActive(false);
+        charName.text = "General Rich";
+        textArea.text = "";
+        continueButton.SetActive(false);
+        textBox.SetActive(false);
+        charGeneralRich.SetActive(true);
+        yield return new WaitForSeconds(2);
+        textBox.SetActive(true);
+        string sentence = "...That's some news";
+        StartCoroutine(Type(sentence));
+        yield return new WaitUntil(() => currText == sentence);
+        eventCount++;
+        continueButton.SetActive(true);
+    }
+
+    IEnumerator EventNine()
+    {
+        // eventCount 8
+        charGeneralRich.SetActive(false);
+        charName.text = "Prince Edgar";
+        textArea.text = "";
+        continueButton.SetActive(false);
+        string sentence = "...What am I supposed to do?";
+        StartCoroutine(Type(sentence));
+        yield return new WaitUntil(() => currText == sentence);
+        eventCount++;
+        continueButton.SetActive(true);
+    }
+
+    IEnumerator EventTen()
+    {
+        // eventCount 9
+        continueButton.SetActive(false);
+        textBox.SetActive(false);
+        yield return new WaitForSeconds(0.3f);
+        choicePanel.SetActive(true);
+    }
+
+    public void ContinueButton()
+    {
+        switch (eventCount)
+        {
+            case 1:
+                StartCoroutine(EventTwo());
+                break;
+            case 2:
+                StartCoroutine(EventThree());
+                break;
+            case 3:
+                StartCoroutine(EventFour());
+                break;
+            case 4:
+                StartCoroutine(EventFive());
+                break;
+            case 5:
+                StartCoroutine(EventSix());
+                break;
+            case 6:
+                StartCoroutine(EventSeven());
+                break;
+            case 7:
+                StartCoroutine(EventEight());
+                break;
+            case 8:
+                StartCoroutine(EventNine());
+                break;
+            case 9:
+                StartCoroutine(EventTen());
+                break;
+        }
+
+    }
+
+    public void LoadScene2Variant(int index)
+    {
+        SceneManager.LoadScene($"Scene2_{index}");
+    }
+   
 
 }
